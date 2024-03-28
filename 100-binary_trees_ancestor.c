a2bf0a1bcc8b6c9dd17c3b3d9666d82bf1ad3025
Brain-Effects <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@github.coom>
2024-03-28 06:42:15 -0700

    Lowest common ancestor

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ *
+ * Return: pointer to the lowest common ancestor node of the two given nodes.
+ * If no common ancestor was found, return NULL
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *mover = (binary_tree_t *)first;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	while (mover)
+	{
+	binary_tree_t *runner = (binary_tree_t *)second;
+
+	while (runner)
+	{
+		if (mover == runner)
+		return (mover);
+		runner = runner->parent;
+	}
+		mover = mover->parent;
+	}
+
+	return (NULL);
+}
