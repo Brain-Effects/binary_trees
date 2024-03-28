cf5b2fa2deda87ec1eac5d75f2a8e65cb948e11b
Brain-Effects <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@github.coom>
2024-03-28 07:14:20 -0700

    Level-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -63,6 +63,5 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 void binary_tree_print_level(const binary_tree_t *tree, void (*func)(int), size_t level);
-void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* _BINARY_TREES_H */
